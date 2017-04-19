99b50395454662e7214fe336d0cd0f13ffc17217
garet999 <garet999@gmail.com>
2017-04-19 21:43:52 +0200

    Added bonus pressure damage.

--- src/Plugins/PaladinBonus.c
+++ src/Plugins/PaladinBonus.c
@@ -3,17 +3,17 @@
 //===== By: ==================================================
 //= Jeroen
 //===== Current Version: =====================================
-//= 0.5 (Beta Testing, for now Non renewal only)
+//= 1.0
 //===== Description: =========================================
 //= bAddWeightDamage increases damage to Shield chain and Shield Boomerang as if the shield has more weight.
+//= bAddPressureDamage increases damage to Pressure
 //===== Additional Comments: =================================
 //= Format:
 //= bonus bAddWeightDamage, x;	// x = Bonus weight
 //= e.g:
 //=   bonus bAddWeightDamage, 2000; // Increases weight by 200 (Virtually) 
 //===== Repo Link: ===========================================
-//= https://github.com/garet999/HerculesPlugins
-//============================================================
+//= https://github.com/garet999
 
 #include "common/hercules.h"
 
@@ -51,7 +51,7 @@
 HPExport struct hplugin_info pinfo = {
 	"Paladin Bonuses",
 	SERVER_TYPE_MAP,
-	"0.5",
+	"0.7",
 	HPM_VERSION,
 };
 
@@ -60,9 +60,11 @@ HPExport struct hplugin_info pinfo = {
 * don't mess with this.
 */
 int bAddWeightDamage = -1;
+int bAddPressureDamage = -1;
 
 struct paladin_bonus_data {
 	int bAddWeightDamage;
+	int bAddPressureDamage;
 };
 
 struct paladin_bonus_data* bonus_search(struct map_session_data* sd) {
@@ -70,6 +72,7 @@ struct paladin_bonus_data* bonus_search(struct map_session_data* sd) {
 	if ((data = getFromMSD(sd, 0)) == NULL) {
 		CREATE(data, struct paladin_bonus_data, 1);
 		data->bAddWeightDamage = 0; // default
+		data->bAddPressureDamage = 0; // default
 		addToMSD(sd, data, 0, true);
 	}
 	return data;
@@ -79,25 +82,44 @@ struct Damage battle_calc_weapon_attack_post(struct Damage wd, struct block_list
 	struct map_session_data *sd;
 	struct paladin_bonus_data *data;
 	struct status_change *sc = status->get_sc(src);
-	sd = BL_CAST(BL_PC, src);
-	data = bonus_search(sd);
 	if (skill_id != PA_SHIELDCHAIN && skill_id != CR_SHIELDBOOMERANG) { // If it isn't about these skills or if the character doesn't have the bonus, drop it!, drop it!
 		return wd;
 	}
-
+	sd = BL_CAST(BL_PC, src);
+	data = bonus_search(sd);
 	if (sd && data->bAddWeightDamage != 0) { //Lets do this!
 		wd.damage += ((data->bAddWeightDamage / 10) * ((sc && sc->data[SC_SOULLINK] && sc->data[SC_SOULLINK]->val2 == SL_CRUSADER && skill_id != PA_SHIELDCHAIN)? 2 : 1));
 	}
 
 	return wd;
+
+}struct Damage battle_calc_misc_attack_post(struct Damage md, struct block_list *src, struct block_list *target, uint16 skill_id, uint16 skill_lv, int mflag) {
+	struct map_session_data *sd;
+	struct paladin_bonus_data *data;
+	struct status_change *sc = status->get_sc(src);
+	if (skill_id != PA_PRESSURE) {
+		return md;
+	}
+
+	sd = BL_CAST(BL_PC, src);
+	data = bonus_search(sd);
+
+	if (sd && data->bAddPressureDamage != 0) { //Lets do this!
+		md.damage += data->bAddPressureDamage;
+	}
+
+	return md;
 }
 
 int pc_bonus_pre(struct map_session_data **sd, int *type, int *val)
 {
-	char entry_name[10];
+	struct paladin_bonus_data *data = bonus_search(*sd);
 	if (*type == bAddWeightDamage) {
-		struct paladin_bonus_data *data = bonus_search(*sd);
 		data->bAddWeightDamage += *val;
+		hookStop();
+	}
+	else if (*type == bAddPressureDamage){
+		data->bAddPressureDamage += *val;
 		hookStop();
 	}
 
@@ -110,18 +132,23 @@ int status_calc_pc_pre(struct map_session_data **sd, enum e_status_calc_opt *opt
 	struct paladin_bonus_data *data = NULL;
 	if ((data = getFromMSD(*sd, 0)) != NULL) {
 		data->bAddWeightDamage = 0; //Default
+		data->bAddPressureDamage = 0; //Default
 	}
 	return 1;
 }
 
 HPExport void plugin_init(void) {
 	bAddWeightDamage = map->get_new_bonus_id();
+	bAddPressureDamage = map->get_new_bonus_id();
 	script->set_constant("bAddWeightDamage", bAddWeightDamage, false, false);
-
+	script->set_constant("bAddPressureDamage", bAddPressureDamage, false, false);
+	
 	addHookPre(pc, bonus, pc_bonus_pre);
 	addHookPre(status, calc_pc_, status_calc_pc_pre);
-//  addHookPost(battle, calc_weapon_damage, battle_calc_weapon_damage_post);
-	addHookPost(battle, calc_weapon_attack, battle_calc_weapon_attack_post);
+	/* TODO: Calc weapon damage post for renewal! */
+	//addHookPost(battle, calc_weapon_damage, battle_calc_weapon_damage_post); // Shield weight Renewal TODO
+	addHookPost(battle, calc_weapon_attack, battle_calc_weapon_attack_post); // For The shield weight Pre-renewal TODO: add guard
+	addHookPost(battle, calc_misc_attack, battle_calc_misc_attack_post); // For pressure
 }
 
 HPExport void server_online(void) {
